c6bcda83e96674b2a761f92e2f75fcda21374266
梁庆 <245374011@qq.com>
2023-08-14 15:13:04 +0800

    修改组的接口参数

--- app/business/entity/iot_gateway_group.go
+++ app/business/entity/iot_gateway_group.go
@@ -1,7 +1,7 @@
 package entity
 
 type GatewayGroupAddOrUpdate struct {
-	Id string `json:"id" description:"组ID，无值为添加，有值则是修改"`
-	Name string `json:"name" description:"组别名"`
-	Mode int `json:"mode" description:"模式"`
+	Id    string `json:"id" description:"组ID，无值为添加，有值则是修改"`
+	Alias string `json:"alias" description:"组别名"`
+	Mode  int    `json:"mode" description:"模式"`
 }

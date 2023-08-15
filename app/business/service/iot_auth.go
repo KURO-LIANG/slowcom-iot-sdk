81ddce10f23eb542232d99e6c9a4390efb0078bf
梁庆 <245374011@qq.com>
2023-08-15 15:08:00 +0800

    更新第三方配置回调auth鉴权接口

--- app/business/service/iot_auth.go
+++ app/business/service/iot_auth.go
@@ -1,8 +1,12 @@
 package service
 
 import (
+	"crypto/md5"
+	"fmt"
 	"github.com/kuro-liang/slowcom-iot-sdk/app/business/entity"
 	"github.com/kuro-liang/slowcom-iot-sdk/http"
+	"strconv"
+	"time"
 )
 
 type IotAuthRequest struct {
@@ -10,8 +14,16 @@ type IotAuthRequest struct {
 }
 
 // AuthSetting 第三方请求回调鉴权配置
-func (s *IotAuthRequest) AuthSetting(req *entity.IotAuthSettingReq) (resData *entity.IotAuthSettingRes, err error) {
-	res, err := s.IotClient.PostJson("/v1/api/third/conf/view", req)
+func (s *IotAuthRequest) AuthSetting(req entity.IotAuthSettingData) (resData *entity.IotAuthSettingRes, err error) {
+	timestamp := strconv.FormatInt(time.Now().UnixNano()/1e9, 10)
+	sign := fmt.Sprintf("%x", md5.Sum([]byte(s.IotClient.ClientId+timestamp)))
+	var request = entity.IotAuthSettingReq{
+		ClientID:  s.IotClient.ClientId,
+		Timestamp: timestamp,
+		Sign:      sign,
+		Data:      req,
+	}
+	res, err := s.IotClient.PostJson("/v1/api/third/conf/view", request)
 	if err == nil {
 		resData = res.Data.(*entity.IotAuthSettingRes)
 	}

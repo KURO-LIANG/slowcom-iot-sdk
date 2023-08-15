1bcecfcaf5fc56251e78874b02aacee6ccaa6a81
梁庆 <245374011@qq.com>
2023-08-15 09:30:08 +0800

    新增了获取组信息接口

--- app/business/service/gateway_group.go
+++ app/business/service/gateway_group.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"fmt"
 	"github.com/kuro-liang/slowcom-iot-sdk/app/business/entity"
 	"github.com/kuro-liang/slowcom-iot-sdk/http"
 )
@@ -21,5 +22,11 @@ func (s *GatewayGroupRequest) Save(req *entity.GatewayGroupAddOrUpdate) (resData
 // Delete 删除组
 func (s *GatewayGroupRequest) Delete(req *entity.GatewayGroupDelete) (res *http.IotRes, err error) {
 	res, err = s.IotClient.PostJson("/device/group/split/delete", req)
+	return
+}
+
+// GroupDetail 查询组信息
+func (s *GatewayGroupRequest) GroupDetail(groupId string) (res *http.IotRes, err error) {
+	res, err = s.IotClient.Get(fmt.Sprint("/device/group/room/detail/", groupId))
 	return
 }

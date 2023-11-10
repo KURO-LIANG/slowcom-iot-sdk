2077560dfba20d47651617e2826c5368c8ca5e92
梁庆 <245374011@qq.com>
2023-11-10 17:03:05 +0800

    增加设备日志请求

--- app/business/service/iot_device.go
+++ app/business/service/iot_device.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"fmt"
 	"github.com/kuro-liang/slowcom-iot-sdk/http"
 )
 
@@ -11,5 +12,11 @@ type DeviceRequest struct {
 // ModeList 获取设备型号列表
 func (s *DeviceRequest) ModeList() (res *http.IotRes, err error) {
 	res, err = s.IotClient.Get("/device/model/list")
+	return
+}
+
+// DeviceLog 获取设备日志列表 按时间倒叙
+func (s *DeviceRequest) DeviceLog(gatewayId string, deviceId string, page int, limit int) (res *http.IotRes, err error) {
+	res, err = s.IotClient.Get(fmt.Sprintf("/device/logs/status/gdid/%s/%s/%d/%d", gatewayId, deviceId, page, limit))
 	return
 }

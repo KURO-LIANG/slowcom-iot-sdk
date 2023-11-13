b00f6fcbd0e77c7745c4a632629c275f9bad3067
梁庆 <245374011@qq.com>
2023-11-13 16:05:57 +0800

    增加取消订阅接口

--- app/business/service/subscribe.go
+++ app/business/service/subscribe.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"fmt"
 	"github.com/kuro-liang/slowcom-iot-sdk/app/business/entity"
 	"github.com/kuro-liang/slowcom-iot-sdk/http"
 )
@@ -11,5 +12,10 @@ type GatewaySubscribeRequest struct {
 
 func (s *GatewaySubscribeRequest) Subscribe(req *entity.SubscribeReq) (res *http.IotRes, err error) {
 	res, err = s.IotClient.PostJson("/device/subscribeMessages/subscribe", req)
+	return
+}
+
+func (s *GatewaySubscribeRequest) UnSubscribe(groupId string, uuid string) (res *http.IotRes, err error) {
+	res, err = s.IotClient.Get(fmt.Sprintf("/device/subscribeMessages/delete/%s/%s", groupId, uuid))
 	return
 }

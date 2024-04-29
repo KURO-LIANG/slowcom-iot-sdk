b70e9222246d7cad6705aafb97d8a2e1439c2265
梁庆 <245374011@qq.com>
2024-04-29 16:56:04 +0800

    修改睡眠带数据

--- app/business/entity/iot_health.go
+++ app/business/entity/iot_health.go
@@ -1,5 +1,12 @@
 package entity
 
+type SleepRecordResult struct {
+	Code         int           `json:"code"`
+	ErrorMessage string        `json:"errorMessage"`
+	Message      string        `json:"message"`
+	Data         []SleepReport `json:"data"`
+}
+
 type SleepReport struct {
 	ID         int    `json:"id"`
 	SN         string `json:"sn"`
@@ -9,23 +16,23 @@ type SleepReport struct {
 }
 
 type Detail struct {
-	Score         Count    `json:"score"`
-	TotalDuration int      `json:"total_duration"`
-	InBedDuration int      `json:"inbed_duration"`
-	GoBedTime     string   `json:"gobed_time"`
-	OutBedTime    string   `json:"outbed_time"`
-	OutBedCount   Count    `json:"outbed_count"`
-	MoveCount     Count    `json:"move_count"`
-	SnoringCount  Count    `json:"snoring_count"`
-	SleepEff      Count    `json:"sleep_eff"`
-	DtArr         []string `json:"dt_arr"`
-	RhArr         []int    `json:"rh_arr"`
-	HxArr         []int    `json:"hx_arr"`
-	SnoringArr    []int    `json:"snoring_arr"`
-	OutBedArr     []int    `json:"outbed_arr"`
-	MoveArr       []int    `json:"move_arr"`
-	HxStopArr     []int    `json:"hxstop_arr"`
-	SleepArr      []int    `json:"sleep_arr"`
+	Score         Count       `json:"score"`
+	TotalDuration int         `json:"total_duration"`
+	InBedDuration int         `json:"inbed_duration"`
+	GoBedTime     string      `json:"gobed_time"`
+	OutBedTime    string      `json:"outbed_time"`
+	OutBedCount   Count       `json:"outbed_count"`
+	MoveCount     Count       `json:"move_count"`
+	SnoringCount  Count       `json:"snoring_count"`
+	SleepEff      interface{} `json:"sleep_eff"`
+	DtArr         []string    `json:"dt_arr"`
+	RhArr         []int       `json:"rh_arr"`
+	HxArr         []int       `json:"hx_arr"`
+	SnoringArr    []int       `json:"snoring_arr"`
+	OutBedArr     []int       `json:"outbed_arr"`
+	MoveArr       []int       `json:"move_arr"`
+	HxStopArr     []int       `json:"hxstop_arr"`
+	SleepArr      []int       `json:"sleep_arr"`
 }
 
 type Count struct {
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/kuro-liang/slowcom-iot-sdk/app/business/entity"
 	"github.com/kuro-liang/slowcom-iot-sdk/app/business/service"
@@ -29,16 +30,33 @@ func main() {
 		Password:     "AE83F1A6F8B14D31EF4B00137756DF5F",
 		AccessToken:  "eyJhbGciOiJSUzI1NiIsImtpZCI6ImNlcnRfZ2RsenU2IiwidHlwIjoiSldUIn0.eyJvd25lciI6IlNsb3djb20iLCJuYW1lIjoib3Blbl9mbnhnYjQiLCJpc3MiOiJodHRwczovL2F1dGguc2xvd2NvbS5jbiIsInN1YiI6ImJjNDkxMDc1LTI2MDUtNDM1Mi04MTkwLTlkYWZkZjYwMzAyMyIsImF1ZCI6WyJkZWUyN2Y5MWRkNjAyMzYyOWVjNyJdLCJleHAiOjE2OTM0NjkxODQsIm5iZiI6MTY5Mjg2NDM4NCwiaWF0IjoxNjkyODY0Mzg0fQ.kUdviZLiDkjq6Mlm1VNUDUgi8WFfjZOLMRWtuz0Nsgu5xAW7Lw9U1XOCaRMq5kI_c_xvmqzV_vC4Lux75lPei9SQByVfGl6ygdbR7k0Gp7Klbl0BPbapZVBgF-m7tfRiIaw5sWAQ2mACQeifEjnz27769Vb5xNCmVWsd3EaQcYMJMLo3WU0g56nOo_fFQ0L7HiOzqdqMSaFK9V0VUKkt0uiEjUlEwrt2bmWimyLu2OGmPxh3Y8lV7jQ7GIalfAvsRHhklEGN9ExGPe74l2g1y5AY2Tg5OvEN7pkwXKhWWyAAFXwUe60GBg_v_KdTwxBo5YbOTfFA2ew5jBZjTFgU7KDHlIc4wIal4hzAVQb09CaWuGOKtdY9CU1zYkQqqjtiP0dW7FCJxr0NfgcCesvlGt491M2iK-FaIevPUgTop4DcbdvpjnVvpX-g5IUgil0Bl-d8fonyAiGBBR1LVSSoQNJCsqdtd-89SaSCFyrPa0ebfco19J4TZHdRDN7EvB-pkaa78qawXU7pnnGrnlF3ZDUvctFwebOKgYApUc71e9MBh0r41mwfJwlq-A5USf2Hi1OGH3_BnUT27il8Etns-1HL-3sokjQZB2in_qgP4q8W3zSWnqS5Sx3CYaWrSIBdi5Ps27FdYSoHNlBELpZEfqNzbAtpQmS660H0KsSgOJk",
 	}
-	request1 := service.GatewayGroupRequest{IotClient: client}
+	request1 := service.HealthRequest{IotClient: client}
 	//
-	res1, err := request1.Save(&entity.GatewayGroupAddOrUpdate{
-		Id:    "64e71104d1e20900011f34ea",
-		Alias: "IOT SDK测试接口11111",
-		Mode:  0,
-	})
+	res1, err := request1.GetSleepReport("84F70369AB54", 1)
 	if err != nil {
 		fmt.Println("请求错误：", err)
 		return
 	}
-	fmt.Printf("结果： %+v\n", res1)
+	bs, e1 := json.Marshal(res1)
+	if e1 != nil {
+		fmt.Println("解析错误：", e1)
+		return
+	}
+	var sleepRecordRes entity.SleepRecordResult
+	err = json.Unmarshal(bs, &sleepRecordRes)
+	if err != nil {
+		fmt.Println("解析错误：", err)
+		return
+	}
+	if sleepRecordRes.Data == nil || len(sleepRecordRes.Data) == 0 {
+		fmt.Println("没有数据")
+		return
+	}
+	var detail entity.Detail
+	err = json.Unmarshal([]byte(sleepRecordRes.Data[0].Detail), &detail)
+	if err != nil {
+		fmt.Println("解析错误：", err)
+		return
+	}
+	fmt.Printf("睡眠带报告：%+v", detail)
 }
